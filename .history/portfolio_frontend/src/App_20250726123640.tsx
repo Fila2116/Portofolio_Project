3c1a0673d2ffa0bd5a1849a8a548800dbb0113fa
Filimon Tesfaye <filimont448@gmail.com>
2025-07-27 16:00:47 +0300

    fix:remove unused variables for Typescript build

--- .history/portfolio_frontend/src/App_20250726123640.tsx
+++ .history/portfolio_frontend/src/App_20250726123640.tsx
@@ -1,10 +1,12 @@
-
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
 import ProfileSection from './components/ProfileSection'
 import ProjectSection from './components/ProjectSection'
 
 function App() {
-  
+  const [count, setCount] = useState(0)
 
   return (
     <>
--- .history/portfolio_frontend/src/main_20250727143518.tsx
+++ .history/portfolio_frontend/src/main_20250727143518.tsx
@@ -5,7 +5,10 @@ import Home from './pages/Home'
 import About from './pages/About'
 import Projects from './pages/Projects'
 import NavBar from './components/NavBar.tsx'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import './index.css'
+import App from './App.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
